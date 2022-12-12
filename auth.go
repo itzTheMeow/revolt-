b478d388d54b2921b3d47da4741f7a17282c85f5
Meow <50887230+itzTheMeow@users.noreply.github.com>
2022-12-12 15:55:31 -0500

    try login

--- auth.go
+++ auth.go
@@ -1,6 +1,11 @@
 package main
 
-import "github.com/pterm/pterm"
+import (
+	"os"
+
+	"github.com/5elenay/revoltgo"
+	"github.com/pterm/pterm"
+)
 
 func checkLogin() {
 	if getLoginToken() != "" {
@@ -10,8 +15,29 @@ func checkLogin() {
 	Clear()
 	pterm.DefaultHeader.WithFullWidth().Println(Config().brandName + " - Log In")
 	pterm.Println()
-	res, _ := pterm.DefaultInteractiveTextInput.WithTextStyle(pterm.NewStyle()).Show(" Email")
-	pterm.Println(res)
-	res2, _ := pterm.DefaultInteractiveTextInput.Show(" Password")
-	pterm.Println(res2)
+	email, _ := pterm.DefaultInteractiveTextInput.Show(" Email")
+	pass, _ := pterm.DefaultInteractiveTextInput.Show(" Password")
+	Clear()
+
+	loader, _ := pterm.DefaultSpinner.Start("Logging in...")
+
+	client := revoltgo.Client{
+		SelfBot: &revoltgo.SelfBot{
+			Email:    email,
+			Password: pass,
+		},
+	}
+
+	err := client.Auth()
+	if err != nil {
+		loader.Fail("Failed to login: " + err.Error())
+		res, _ := pterm.DefaultInteractiveConfirm.WithDefaultValue(true).Show("Try again?")
+		if res {
+			checkLogin()
+		} else {
+			os.Exit(0)
+		}
+	}
+
+	loader.Success()
 }
